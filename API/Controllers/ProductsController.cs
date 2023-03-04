729ed7dc4c16c89bcde2c4f53679e8fb9535860f
Felix Battig <fbattig@yahoo.com>
2023-03-04 13:47:42 -0500

    Section 3

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        {
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            var prodctBrands = await _repo.GetProductBrandsAsync();
+            return Ok(prodctBrands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyCollection<ProductType>>> GetProductTypes()
+        {
+            var productType = await _repo.GetProductTypesAsync();
+            return Ok(productType);
+
+        }
+
+
+
+    }
+}
