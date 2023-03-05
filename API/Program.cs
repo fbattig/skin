e9f409bc56069484d94364d2bb7dec3438823d8c
Felix Battig <fbattig@yahoo.com>
2023-03-05 14:40:49 -0500

    Section 4 - API Generic Repository

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,9 @@ builder.Services.AddDbContext<StoreContext>(opt => {
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
