1caf5b6dd180971b0546daf69e8744d9c904038a
Felix Battig <fbattig@yahoo.com>
2023-03-08 06:18:23 -0500

    Cors

--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,8 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
     app.UseSwaggerUI();
 
 app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
